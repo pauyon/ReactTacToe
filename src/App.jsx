e7b1cb15d895c3efdfd13a82bd97626ff55864b6
Paulino Auyon <pauyon@jgboswell.com>
2025-06-02 12:03:44 -0700

    Add player name component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import Player from "./components/Player"
 
 function App() {
   
 
   return (
     <main>
-      <div id="come-container">
-        PLAYERS
+      <div id="game-container">
+        <ol id="players">
+          <Player initialName="Player 1" symbol="X" />
+          <Player initialName="Player 2" symbol="O" />
+        </ol>
 
         GAME BOARD
       </div>
---
+++ src/components/Player.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+    const [playerName, setPlayerName] = useState(initialName);
+    const [isEditing, setIsEditing] = useState(false);
+
+    function handleEditClick() {
+        // setIsEditing(!isEditing); // schedules a state update, but it may not reflect the latest state immediately
+        setIsEditing((editing) => !editing); // schedules a state update and reflects the latest state immediately
+    }
+
+    function handleChange(event) {
+        setPlayerName(event.target.value);
+    }
+
+    let buttonName = "Edit";
+    let editablePlayerName = <span className="player-name">{playerName}</span>;
+
+    if (isEditing) {
+
+        buttonName = "Save";
+        editablePlayerName = (
+            <input 
+            type="text" 
+            defaultValue={playerName}
+            onChange={handleChange} />
+        );
+    }
+
+    return (
+        <li>
+            <span className="player">
+                {editablePlayerName}
+                <span className="player-symbol">{symbol}</span>
+            </span>
+            <button onClick={handleEditClick}>{buttonName}</button>
+        </li>
+    );
+}
