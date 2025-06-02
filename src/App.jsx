77966a28e5aa443b5777e8c8131a7daf9151fc28
Paulino Auyon <pauyon@jgboswell.com>
2025-06-02 13:32:08 -0700

    Generate logs from derived state for logs

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ function App() {
           onSelectSquare={handleSelectSquare}
           turns={gameTurns} />
       </div>
-      <Log />
+      <Log turns={gameTurns} />
     </main>
   )
 }
--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -1,7 +1,11 @@
-export default function Log(){
-    
-
+export default function Log({ turns }){
     return (
-        <ol id = "log"></ol>
+        <ol id = "log">
+            {turns.map((turn) => (
+                <li key={`${turn.square.row}${turn.square.col}`}>
+                    {turn.player} selected {turn.square.row + 1}, {turn.square.col + 1}
+                </li>
+            ))}
+        </ol>
     );
 }
