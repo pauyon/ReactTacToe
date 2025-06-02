4cf0904a7582cc6e06d4b47f613ca308cc9243b6
Paulino Auyon <pauyon@jgboswell.com>
2025-06-02 12:45:54 -0700

    Add gameboard logic for displaying player moves and turns

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,18 +1,32 @@
+import { useState } from "react";
+
 const initialGameBoard = [
     [null, null, null],
     [null, null, null],
     [null, null, null]
 ];
 
-export default function GameBoard() {
+export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
+    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+    function handleSelectSqure(rowIndex, colIndex) {
+        setGameBoard((prevGameBoard) => {
+            const updatedGameBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+            updatedGameBoard[rowIndex][colIndex] = activePlayerSymbol;
+            return updatedGameBoard;
+        });
+
+        onSelectSquare();
+    }
+
     return (
         <ol id="game-board">
-            {initialGameBoard.map((row, rowIndex) => (
+            {gameBoard.map((row, rowIndex) => (
                 <li key={rowIndex}>
                     <ol>
                         {row.map((playerSymbol, colIndex) => (
                             <li key={colIndex}>
-                                <button>
+                                <button onClick={() => handleSelectSqure(rowIndex, colIndex)}>
                                     {playerSymbol}
                                 </button>
                             </li>
